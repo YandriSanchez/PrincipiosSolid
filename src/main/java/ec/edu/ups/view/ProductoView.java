db7bc26de31e6b2e18534599a7078f5d68d8e61e
yandri140203 <yandri.sanchez1402@gmail.com>
2025-06-13 11:44:05 -0500

    adelanto del proyecto

---
+++ src/main/java/ec/edu/ups/view/ProductoView.java
@@ -0,0 +1,19 @@
+package ec.edu.ups.view;
+
+import javax.swing.*;
+
+public class ProductoView {
+    private JTextField txtcodigo;
+    private JTextField txtnombre;
+    private JTextField txtprecio;
+    private JButton btnMostrar;
+    private JButton btnAgregar;
+    private JLabel titulo;
+    private JPanel contPanel;
+    private JPanel PanelSuperior;
+    private JPanel PanelMedio;
+    private JPanel PanelInferior;
+    private JLabel mensaje1;
+    private JLabel mensaje2;
+    private JLabel mensaje3;
+}
